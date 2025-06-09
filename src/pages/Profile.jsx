a7d986a9f1e86a4695540c1436abdc5780790e3d
Amrita Chanda <amrita.chanda@gmail.com>
2025-06-09 12:18:51 +0530

    Debug: profile page not showing up in prod

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,16 +1,15 @@
-import React from 'react'
 import Header from '../components/Header'
 import { useAuth } from '../utils/AuthContext'
 
 const Profile = () => {
     const { user } = useAuth()
     return (
-        <>
+        <div>
             <Header currentRoute="/profile" />
             <div className="container">
                 <h1>{user.name}, this is your Profile.</h1>
             </div>
-        </>
+        </div>
     )
 }
 
